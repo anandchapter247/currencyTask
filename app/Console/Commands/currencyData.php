f2bdf335fdacacd151ed9279e04f9c7c78bf11f0
ankit.singh <ankit.singh@chapter247.com>
2020-11-27 18:29:21 +0530

    manage curreny record by commond

---
+++ app/Console/Commands/currencyData.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Currency;
+use App\Models\CurrencyHistory;
+use Orchestra\Parser\Xml\Facade as XmlParser;
+
+class currencyData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'add:currency';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'daily currency price are added';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $xml = file_get_contents('http://www.cbr.ru/scripts/XML_daily.asp');
+        $xml = simplexml_load_string($xml);
+             
+        if (!empty($xml)) {
+            foreach ($xml as $key=>$value) {
+                if (!Currency::where('ID', $value[0]['ID'])->count()) {
+                    $data = new Currency;
+                    $data->ID = $value[0]['ID'];
+                    $data->NumCode = $value->NumCode;
+                    $data->CharCode = $value->CharCode;
+                    $data->Nominal = $value->Nominal;
+                    $data->Name = $value->Name;
+                    $data->save();
+                }
+                if (!CurrencyHistory::where('currency_date', $xml['Date'])->where('currency_id', $value->NumCode)->count()) {
+                    $dataHistory = new CurrencyHistory;
+                    $dataHistory->currency_id = $value->NumCode;
+                    $dataHistory->price = $value->Value;
+                    $dataHistory->currency_date = $xml['Date'];
+                    $dataHistory->save();
+                }
+                
+            }
+        } 
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,31 +19,7 @@ class DatabaseSeeder extends Seeder
     {
         if (!User::count()) {
             \App\Models\User::factory(1)->create();
-        }        
-
-        $xml = file_get_contents('http://www.cbr.ru/scripts/XML_daily.asp');
-        $xml = simplexml_load_string($xml);
-             
-        if (!empty($xml)) {
-            foreach ($xml as $key=>$value) {
-                if (!Currency::where('ID', $value[0]['ID'])->count()) {
-                    $data = new Currency;
-                    $data->ID = $value[0]['ID'];
-                    $data->NumCode = $value->NumCode;
-                    $data->CharCode = $value->CharCode;
-                    $data->Nominal = $value->Nominal;
-                    $data->Name = $value->Name;
-                // $data->Value = $value->Value;
-                    $data->save();
-                }
-
-                $dataHistory = new CurrencyHistory;
-                $dataHistory->currency_id = $value->NumCode;
-                $dataHistory->price = $value->Value;
-                $dataHistory->currency_date = $xml['Date'];
-                $dataHistory->save();
-            }
-        }     
+        }              
     }
 }
 
